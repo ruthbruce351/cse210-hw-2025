8fe598803e5018ccda815529258378a386bace2e
Ruth Blake <ruthblake@Ruths-Air.lan>
2025-01-08 17:31:38 -0600

    Core Requirement 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else
+        {
+            Console.WriteLine("You got an F!");
+        }
     }
 }
