68a879d4a0126c8a76e7b494609fab7b0f94c462
Ruth Blake <ruthblake@Ruths-Air.lan>
2025-01-10 19:20:18 -0600

    Added menu for Journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        string choice = "";
+        do
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            choice = Console.ReadLine();
+
+        } while (choice != "5");
     }
 }
