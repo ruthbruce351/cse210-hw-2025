06915390ba8966a77ff08dadb56b670163aafa74
Ruth Blake <ruthblake@Ruths-Air.lan>
2025-01-10 19:11:02 -0600

    Created stubs for Journal Class

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+    
+    public void AddNewEntry()
+    {
+        
+    }
+
+    public void DisplayAll()
+    {
+        
+    }
+
+    public void SaveToFile()
+    {
+        
+    }
+
+    public void LoadFromFile()
+    {
+        
+    }
+}
